2a38d74303583df0686558bc5c3da700554929f1
Nick Lauritzen <sirnicklaus@gmail.com>
2022-12-27 09:51:56 -0700

    Progress 5

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 let selectedRating = document.querySelector('#numButtonDiv');
 
-console.log(numButtonDiv);
+let one = document.querySelector('#one');
+let two = document.querySelector('#two');
+let three = document.querySelector('#three');
+let four = document.querySelector('#four');
+let five = document.querySelector('#five');
+
+let storedRating = '0';
 
 selectedRating.addEventListener('click', (e) => {
-    let storedRating = '0';
     if (e.target.innerText == '1') {
         storedRating = '1';
     } else if (e.target.innerText == '2') {
@@ -12,13 +17,81 @@ selectedRating.addEventListener('click', (e) => {
         storedRating = '3';
     } else if (e.target.innerText == '4') {
         storedRating = '4';
-    } else storedRating = '5';
+    } else if (e.target.innerText == '5') {
+        storedRating = '5';
+    };
     console.log(storedRating);
+    
+    if (storedRating == '1') {
+        one.style.backgroundColor = 'hsl(217, 12%, 63%)'
+        one.style.color = 'hsl(0, 0%, 100%)'
+    };
+    
+    if (storedRating == '2') {
+        two.style.backgroundColor = 'hsl(217, 12%, 63%)'
+        two.style.color = 'hsl(0, 0%, 100%)'
+    };
+
+    if (storedRating == '3') {
+        three.style.backgroundColor = 'hsl(217, 12%, 63%)'
+        three.style.color = 'hsl(0, 0%, 100%)'
+    };
+
+    if (storedRating == '4') {
+        four.style.backgroundColor = 'hsl(217, 12%, 63%)'
+        four.style.color = 'hsl(0, 0%, 100%)'
+    };
+
+    if (storedRating == '5') {
+        five.style.backgroundColor = 'hsl(217, 12%, 63%)'
+        five.style.color = 'hsl(0, 0%, 100%)'
+    };
+    
+    if (storedRating != '1') {
+        one.style.backgroundColor = 'hsl(216, 12%, 24%)'
+        one.style.color = 'hsl(217, 12%, 63%)'
+    };
+    
+    if (storedRating != '2') {
+        two.style.backgroundColor = 'hsl(216, 12%, 24%)'
+        two.style.color = 'hsl(217, 12%, 63%)'
+    };
+
+    if (storedRating != '3') {
+        three.style.backgroundColor = 'hsl(216, 12%, 24%)'
+        three.style.color = 'hsl(217, 12%, 63%)'
+    };
+
+    if (storedRating != '4') {
+        four.style.backgroundColor = 'hsl(216, 12%, 24%)'
+        four.style.color = 'hsl(217, 12%, 63%)'
+    };
+
+    if (storedRating != '5') {
+        five.style.backgroundColor = 'hsl(216, 12%, 24%)'
+        five.style.color = 'hsl(217, 12%, 63%)'
+    };
 });
 
-var page1 = document.querySelector('#page1');
-var page2 = document.querySelector('#page2');
+let submitButton = document.querySelector('#submitButton');
 
-if (1 === 1) {
-    page1 = page2
-}
+let page1 = document.querySelector('#page1');
+let page2 = document.querySelector('#page2').style.display = 'none';
+
+submitButton.addEventListener('click', (e) => {
+    function whatYouSelected () {
+        if (+storedRating > 0 && +storedRating < 6) {
+            document.querySelector('#youSelectedText').innerText = `You selected ${storedRating} out of 5`;
+            function moveTo2 () {
+                page1.style.display = 'none';
+                // page2.style.display = 'flex';
+                document.querySelector('#page2').style.display = 'flex';
+            };
+        } else {
+            console.log('User did not select a rating.');
+            return;
+        };
+    moveTo2();
+    };
+whatYouSelected();
+});
